d0c32b22ffcf21aa3cfd3ce099ffb4944b3a0062
alexanderamundsen <alexbolgenamundsen@gmail.com>
2025-07-08 11:54:08 +0200

    Update demo section to use new Vercel Blob demo video

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -264,7 +264,7 @@ const Work = () => (
           </VideoItem>
           <VideoItem>
             <VideoPlaceholderBox as="video"
-              src="/video-landing-page.mp4"
+              src="https://ascpxp2rq0hfmacv.public.blob.vercel-storage.com/video-demo-3NiOB5ays19lrZsc6nRpbfMMDQH9OU.mp4"
               autoPlay
               loop
               muted
